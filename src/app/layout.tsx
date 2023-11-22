f2aeeb9e59d6692ac6dfb348cc3cd3db4a82e715
Valerii Lopatko <vlopatko1999@gmail.com>
2023-11-22 06:58:17 +0200

    init commit & setup tech stack & added next-auth & setup Prisma&PlanetScale DB & write NavBar component

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import NavBar from '@/components/NavBar'
+import { Toaster } from '@/components/ui/Toaster'
+import { cn } from '@/lib/utils'
+import '@/styles/globals.css'
+import { Inter } from 'next/font/google'
+
+export const metadata = {
+  title: 'Threaddit',
+  description: 'A Reddit clone built with Next.js and TypeScript.',
+}
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html
+      lang="en"
+      className={cn(
+        'light bg-white text-slate-900 antialiased',
+        inter.className
+      )}
+    >
+      <body className="min-h-screen bg-slate-50 pt-12 antialiased">
+        <NavBar />
+
+        <div className="container mx-auto h-full max-w-7xl pt-12">
+          {children}
+        </div>
+
+        <Toaster />
+      </body>
+    </html>
+  )
+}
---
+++ src/components/SignIn.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react'
+import { Icons } from './Icons'
+import Link from 'next/link'
+import UserAuthForm from './UserAuthForm'
+
+const SignIn: FC = () => {
+  return (
+    <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
+      <div className="flex flex-col space-y-2 text-center">
+        <Icons.logo className="mx-auto h-12 w-12" />
+        <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
+        <p className="mx-auto max-w-xs text-sm">
+          By continuing, you are setting up a Threaddit account and agree to our
+          User Agreement and Privacy Policy.
+        </p>
+
+        <UserAuthForm />
+
+        <p className="px-8 text-center text-sm text-zinc-700">
+          New to Threaddit?{' '}
+          <Link
+            href={'/sign-up'}
+            className="text-sm underline underline-offset-4 hover:text-zinc-800"
+          >
+            Sign up
+          </Link>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default SignIn
